b99d67130016553f65c6b5ce9584183dabfe6a07
YakirTsuberi <tsuberyr@gmail.com>
2017-06-01 13:59:54 +0300

    add button remove connect

--- static/javascript/myjs.js
+++ static/javascript/myjs.js
@@ -4,12 +4,16 @@
 var connections = 1;
 var container = document.getElementById('container');
 function add_connect() {
-    console.log(connections);
     var div = document.createElement('div');
     connections += 1;
     var start_sim = document.getElementsByName('sim_num1')[0].value;
-    console.log(start_sim);
     var html = '<div class="panel panel-primary"><div class="panel-heading"><span>מסלול</span><span class="pull-left">' + connections + '</span></div><div class="panel-body"><input value="' + start_sim + '" class="form-control" name="sim_num' + connections.toString() + '" placeholder="מספר סים" type="text" required="required"/><input class="form-control" name="phone_num' + connections.toString() + '" placeholder="מספר לניוד" type="text" required="required"/></div></div>';
     div.innerHTML = html;
     container.appendChild(div);
+}
+function remove_connect() {
+    if (container.children.length > 1) {
+        container.removeChild(container.lastElementChild);
+        connections -= 1
+    }
 }
