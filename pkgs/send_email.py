51d2dc1e5eb616438e4575f42ff9dba2d1c5c8d7
YakirTsuberi <tsuberyr@gmail.com>
2017-06-22 22:23:08 +0300

    remove tran link

--- pkgs/send_email.py
+++ pkgs/send_email.py
@@ -5,8 +5,8 @@ import yagmail
 
 args = sys.argv[1:]
 to = args[args.index('-t') + 1]
-subject = eval(args[args.index('-s') + 1]).decode('ascii')
-contents = eval(args[args.index('-c') + 1]).decode('ascii')
+subject = eval(args[args.index('-s') + 1]).decode('utf-8')
+contents = eval(args[args.index('-c') + 1]).decode('utf-8')
 yagmail.SMTP('yishaiphone@gmail.com', 'yP1q2w3e4r!').send(to=to,
                                                           subject=subject,
                                                           contents=contents)
