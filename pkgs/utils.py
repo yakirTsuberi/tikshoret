4f10c135cd628d248a5da83dfa3905113e5e2362
YakirTsuberi <tsuberyr@gmail.com>
2017-08-24 15:42:13 +0300

    debug

--- pkgs/utils.py
+++ pkgs/utils.py
@@ -92,6 +92,7 @@ def check_client(_id, first_name, last_name, address, city, phone, email):
 
 
 def check_credit_card(number, month, year, cvv):
+    print(number, month, year, cvv)
     if not number.isdigit() or not month.isdigit() or not year.isdigit() or not cvv.isdigit():
         return False
     if int(month) > 12 or int(month) < 1 or int(year) < 1:
@@ -105,6 +106,8 @@ def check_credit_card(number, month, year, cvv):
             today = datetime.datetime.utcnow()
             credit_date = datetime.datetime(int(year), int(month), 1)
             return today < credit_date
+        print(number, month, year, cvv)
+        print(card.is_mod10_valid, card.is_valid)
         return card.is_mod10_valid and card.is_valid
     return False
 
