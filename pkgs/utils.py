c43d9332b1eb3e2963178693ada1b02e377c0a7e
YakirTsuberi <tsuberyr@gmail.com>
2017-09-19 00:18:12 +0300

    md

--- pkgs/utils.py
+++ pkgs/utils.py
@@ -282,7 +282,7 @@ def write_to_drive(values):
 
 
 def write_to_excel(agent, date) -> Path:
-    path = str(Path.cwd().parent / 'static' / (agent + '.xlsx'))
+    path = str(Path.cwd().parent / 'static' / 'excel_tmp' / (agent + '.xlsx'))
     workbook = xlsxwriter.Workbook(path)
     worksheet = workbook.add_worksheet()
     data = {'חברה': [], 'מסלול': [], 'לקוח': [], 'ת.ז.': [], 'טלפון': [], 'סים': [], 'תאריך': []}
