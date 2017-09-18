7de2d81e2043761a86caff0e06f4517ae721e83b
YakirTsuberi <tsuberyr@gmail.com>
2017-09-18 20:50:29 +0300

    md

--- pkgs/utils.py
+++ pkgs/utils.py
@@ -282,7 +282,7 @@ def write_to_drive(values):
 # noinspection SpellCheckingInspection
 if __name__ == '__main__':
     # set_up_group('yishaiphone-prodaction', 'yakir@ravtech.co.il', '71682547', 'יקיר', 'צוברי')
-    # remove_user('tsuberyr@gmail.com')
+    remove_user('p052760@gmail.com')
     # remove_full_stack_transaction('yakir@ravtech.co.il', '0')
     # _copy_all_tracks()
     pass
