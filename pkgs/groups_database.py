c204c8769ed71c05e63915638e8dae0b51c0e6eb
YakirTsuberi <tsuberyr@gmail.com>
2017-10-01 14:58:54 +0300

    fix icon excel center

--- pkgs/groups_database.py
+++ pkgs/groups_database.py
@@ -450,33 +450,30 @@ if __name__ == '__main__':
     # db = DBGroups('yishaiphone-prodaction').delete_agent('yair.p.86@hotnail.com')
     # db = DBGroups('yishaiphone-prodaction').get_reward(datetime.datetime(2017, 8, 1))
     db = DBGroups('yishaiphone-prodaction')
-    import csv
+    a = []
+    for i in db.session.query(Transactions.agent_id, Transactions.track, Transactions.client_id, Transactions.phone_num,
+                              Transactions.date_time).all():
+        track = db.get_track(_id=i.track)
+        if track.company == 'hot':
+            f = db.session.query(Tags.name).filter(Tags.track_id == i.track).first()
+            if not f:
+                continue
+            if f.name == 'כשר':
+                agent = db.get_agent(i.agent_id)
+                client = db.get_client(i.client_id)
+                cc = db.get_credit_card(i.client_id)
+                if cc:
+                    cc = cc.card_number[-4:]
+                else:
+                    cc = db.get_bank_account(i.client_id).account_num[-4:]
 
-    with open('hot.csv', "w", encoding='utf-8') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        for i in db.session.query(Transactions.agent_id, Transactions.track, Transactions.client_id, Transactions.phone_num,
-                                  Transactions.date_time).all():
-            track = db.get_track(_id=i.track)
-            if track.company == 'hot':
-                f = db.session.query(Tags.name).filter(Tags.track_id == i.track).first()
-                if not f:
-                    continue
-                if f.name == 'כשר':
-                    agent = db.get_agent(i.agent_id)
-                    client = db.get_client(i.client_id)
-                    cc = db.get_credit_card(i.client_id)
-                    if cc:
-                        cc = cc.card_number[-4:]
-                    else:
-                        cc = db.get_bank_account(i.client_id).account_num[-4:]
+                row = [agent.first_name + ' ' + agent.last_name,
+                       client.first_name + ' ' + client.last_name,
+                       i.client_id,
+                       cc,
+                       str(i.phone_num),
+                       str(i.date_time)]
 
-                    row = [agent.first_name + ' ' + agent.last_name,
-                           client.first_name + ' ' + client.last_name,
-                           i.client_id,
-                           cc,
-                           str(i.phone_num),
-                           str(i.date_time)]
-
-                    writer.writerow(row)
-                    print(row)
+                a.append(row)
+    open('hot.text', "w", encoding='utf-8').write(str(a))
     pass
