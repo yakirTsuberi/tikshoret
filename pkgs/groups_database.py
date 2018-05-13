d0737d9dbdc1aa5afa441ea207d28c1cb682f2bd
Yakir Tsuberi <yakir@ravtech.co.il>
2018-05-13 10:26:13 +0300

    add id to sale

--- pkgs/groups_database.py
+++ pkgs/groups_database.py
@@ -460,9 +460,10 @@ if __name__ == '__main__':
     # db = DBGroups('yishaiphone-prodaction').delete_agent('yair.p.86@hotnail.com')
     # db = DBGroups('yishaiphone-prodaction').get_reward(datetime.datetime(2017, 8, 1))
 
-    db = DBGroups('yishaiphone-prodaction')
-    t = db.get_transaction(3370)
-    print(db.update_transactions(t.id, {'date_time': t.date_time.replace(day=2, month=5)}))
-
-    t = db.get_transaction(3457)
-    print(db.update_transactions(t.id, {'status': 0}))
+    # db = DBGroups('yishaiphone-prodaction')
+    # t = db.get_transaction(3370)
+    # print(db.update_transactions(t.id, {'date_time': t.date_time.replace(day=2, month=5)}))
+    #
+    # t = db.get_transaction(3457)
+    # print(db.update_transactions(t.id, {'status': 0}))
+    pass
